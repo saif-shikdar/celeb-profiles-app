be43252a78916998513f272108365351b2396179
Saif Shikdar <43826661+saif-shikdar@users.noreply.github.com>
2025-03-21 13:27:32 +0000

    Created Project Files

---
+++ CelebProfiles/CelebProfilesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CelebProfilesApp.swift
+//  CelebProfiles
+//
+//  Created by Saif Shikdar on 21/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct CelebProfilesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
